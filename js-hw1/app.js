b62e1762dc685bd135cfc871845ec8749cf49bc6
sakdipat <sakdipat5465@gmail.com>
2022-04-29 12:14:52 +0700

    add cal

--- js-hw1/app.js
+++ js-hw1/app.js
@@ -1,6 +1,18 @@
 const displayElement = document.getElementById('calculator-display')
 
+
 const buttonPressed = (character) => {
     displayElement.value = displayElement.value + character
+}
+
+const compute = () => {
+    displayElement.value = eval(displayElement.value)
+}
+
+const clearDisplay = () => {
+    
+}
+
+const deleteLastCharacter = () => {
 
 }
