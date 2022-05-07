c30ccdc691406fc509534c46ef13a5c144d97d63
sakdipat <sakdipat5465@gmail.com>
2022-05-07 18:29:01 +0700

    hw7

--- js-hw7/scripts/app.js
+++ js-hw7/scripts/app.js
@@ -5,27 +5,33 @@ const characterImage = document.getElementById('character-image')
 const findBtn = document.getElementById('find-btn')
 
 findBtn.addEventListener('click', async () => {
+
   try {
     const input = inputName.value
     const url = `https://rickandmortyapi.com/api/character?name=${input}`
-    const response = await (await fetch(url)).json()
+    const response = await(await fetch(url)).json()
 
-    if (!response.results) {
-      throw { message: `${input} not found`}
+    if (!response.results){
+      throw { message: `${input} not found` }
     }
 
     if (!response.results?.length) {
       throw { message: `${input} not found`}
     }
 
+    // console.log(response.results[0])
+
     const character = response.results[0]
 
     characterName.innerHTML = character.name
     characterImage.src = character.image
     characterType.innerHTML = character.species
-  } catch (error) {
+
+  }catch (error) {
     window.alert(`[ERROR] => ${error.message}`)
   } finally {
     inputName.value = ''
   }
+
+
 })
